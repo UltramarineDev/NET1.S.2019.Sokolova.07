efa0bef0c3508d0bea9c4ebba6114c65dac001ef
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-04-03 14:32:34 +0300

    add solution to task 1

---
+++ ArrayManipulations.Tests/ArrayExtensionTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ArrayManipulations.Tests
+{
+    public class ArrayExtensionTests
+    {
+        [TestCase(new int[] { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 }, 7, ExpectedResult = new int[] { 7, 7, 70, 17 })]
+        [TestCase(new int[] { -678, 7, 3, 9, 1, 9, -56 }, 0, ExpectedResult = new int[] { })]
+        [TestCase(new int[] { 3, 315, 9, 0, 45, 0, 32, -98, -535 }, 3, ExpectedResult = new int[] { 3, 315, 32, -535 })]
+        [TestCase(new int[] { 14, 64, 98, 0, -3, 87, 43, 64, 99, 52, 87 }, 4, ExpectedResult = new int[] { 14, 64, 43, 64 })]
+        [TestCase(new int[] { 6, 76, 87, 09, -546, 6, 76, 0, 0, 3 }, 1, ExpectedResult = new int[] { })]
+        public int[] FilterArrayByKeyTest(int[] array, int key)
+        => array.Filter(new FilterArrayByKey(key));
+
+        [Test]
+        public void FilterArrayByKey_InvalidKey_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new FilterArrayByKey(15));
+            Assert.Throws<ArgumentException>(() => new FilterArrayByKey(-15));
+        }
+
+        [Test]
+        public void FilterArrayByKey_EmptyArray_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => ArrayExtension.Filter(new int[] { }, new FilterArrayByKey(8)));
+        }
+
+        [Test]
+        public void FilterArrayByKey_ArrayIsNull_ThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ArrayExtension.Filter(null, new FilterArrayByKey(8)));
+        }
+
+        [TestCase(new int[] { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 }, ExpectedResult = new int[] { 2, 4, 6, 68, 70 })]
+        [TestCase(new int[] { -678, 7, 3, 9, 1, 9, -56 }, ExpectedResult = new int[] { -678, -56 })]
+        [TestCase(new int[] { 3, 315, 9, 0, 45, 0, 32, -98, -535 }, ExpectedResult = new int[] { 0, 0, 32, -98 })]
+        [TestCase(new int[] { 14, 64, 98, 0, -3, 87, 43, 64, 99, 52, 87 }, ExpectedResult = new int[] { 14, 64, 98, 0, 64, 52 })]
+        [TestCase(new int[] { 13, 97, -9853, -7, 9, 5, 55, 321 }, ExpectedResult = new int[] { })]
+        public int[] EvenOrOddTest(int[] array)
+            => array.Filter(new EvenOrOdd());
+
+        [TestCase(new int[] { 0, -98, 989, 4334, -98, 1331, 5543, -876, 1353 }, ExpectedResult = new int[] { 0, 989, 4334, 1331 })]
+        [TestCase(new int[] { 7666, 525, 87, 2222, 1212, 22222222, 3 }, ExpectedResult = new int[] { 525, 2222, 22222222, 3 })]
+        [TestCase(new int[] { 76, -98, 355, 551, 654, 7611, 75611, -8765, 789 }, ExpectedResult = new int[] { })]
+        [TestCase(new int[] { 3103013, 3013103, 103030301 }, ExpectedResult = new int[] { 3103013, 3013103, 103030301 })]
+        [TestCase(new int[] { 60, 80, 990, 1177711 }, ExpectedResult = new int[] { 1177711 })]
+        public int[] Palindrome(int[] array)
+            => array.Filter(new Palindrome());
+    }
+}
---
+++ ArrayManipulations/ArrayExtension.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayManipulations
+{
+    /// <summary>
+    /// Interface represent method FilterByCondition
+    /// </summary>
+    public interface IFilter
+    {
+        int[] FilterByCondition(int[] array);
+    }
+
+    /// <summary>
+    /// ArrayExtension class
+    /// </summary>
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Method filters array using condition
+        /// </summary>
+        /// <param name="array">input array</param>
+        /// <param name="filter">instance of IFilter interface</param>
+        /// <returns>array with numbers satisfying condition</returns>
+        /// <exception cref="System.ArgumentException">Thrown when array is empty</exception>
+        /// <exception cref="System.ArgumentNullException">Thrown when array is null</exception>
+        public static int[] Filter(this int[] array, IFilter filter)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array), "Source array can not be null.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Source array can not be empty.", nameof(array));
+            }
+
+            return filter.FilterByCondition(array);
+        }
+    }
+
+    /// <summary>
+    /// Class FilterArrayByKey with implementation of IFilter interface
+    /// </summary>
+    public class FilterArrayByKey : IFilter
+    {
+        /// <summary>
+        /// Initializes a new instance of the FilterArrayByKey class
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown if key value in invalid</exception>     
+        public FilterArrayByKey(int key)
+        {
+            if (key > 9 || key < 0)
+            {
+                throw new ArgumentException(" Input number is not a digit.", nameof(key));
+            }
+
+            this.Key = key;
+        }
+
+        /// <summary>
+        /// Gets or sets the value of key
+        /// </summary>
+        public int Key { get; set; }
+
+        /// <summary>
+        /// Method creates new array with numbers that contain key 
+        /// </summary>
+        /// <param name="array">input array if integers</param>
+        /// <returns>array of integers</returns>
+        public int[] FilterByCondition(int[] array)
+        {
+            List<int> resultValuesList = new List<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (this.IsDigitInNumber(array[i], this.Key) == true)
+                {
+                    resultValuesList.Add(array[i]);
+                }
+            }
+
+            return resultValuesList.ToArray();
+        }
+
+        private bool IsDigitInNumber(int number, int key)
+        {
+            while (number != 0)
+            {
+                if (Math.Abs(number % 10) == key)
+                {
+                    return true;
+                }
+
+                number = number / 10;
+            }
+
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Class EvenOrOdd with implementation of IFilter interface
+    /// </summary>
+    public class EvenOrOdd : IFilter
+    {
+        /// <summary>
+        /// Method creates new array with even numbers
+        /// </summary>
+        /// <param name="array">input array of integers</param>
+        /// <returns>array of integers</returns>
+        public int[] FilterByCondition(int[] array)
+        {
+            List<int> resultValuesList = new List<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    resultValuesList.Add(array[i]);
+                }
+            }
+
+            return resultValuesList.ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Class Palindrome with implementation of IFilter interface
+    /// </summary>
+    public class Palindrome : IFilter
+    {
+        /// <summary>
+        /// Method creates new array with palindromes
+        /// </summary>
+        /// <param name="array">input array of itegers</param>
+        /// <returns>array of integers</returns>
+        public int[] FilterByCondition(int[] array)
+        {
+            List<int> resultValuesList = new List<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (this.IsPalindrome(array[i]) == true)
+                {
+                    resultValuesList.Add(array[i]);
+                }
+            }
+
+            return resultValuesList.ToArray();
+        }
+
+        private bool IsPalindrome(int number)
+        {
+            string numberInString = number.ToString();
+            for (int i = 0, j = numberInString.Length - 1; i < numberInString.Length; i++, j--)
+            {
+                if (numberInString[i] != numberInString[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    } 
+}
